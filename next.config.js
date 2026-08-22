482e6c854344da674c9c0df835e928d106590871
zenaistockvn <zenai.stock@gmail.com>
2026-08-22 19:20:04 +0700

    chore(images): remove Haravan CDN allowlist

--- next.config.js
+++ next.config.js
@@ -10,10 +10,6 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'images.unsplash.com',
       },
-      {
-        protocol: 'https',
-        hostname: 'product.hstatic.net',
-      },
     ],
   },
   async redirects() {
